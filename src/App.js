5d1319481fe42577162cbee1ed28bbaa4e975aab
Abhirajkar Bajpai <21IT3001@rgipt.ac.in>
2024-12-18 15:54:37 +0530

    frontend change 5 fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import "./App.css";
 import SignupScreen from "./screens/SignupScreen/Signup";
 import LoginScreen from "./screens/LoginScreen/Login";
-import PersonalInfo from "./screens/PersonalInfoScreen/PersonalInfo";
-import Navbar from "./components/Navbar/Navbar";
-import Groups from "./screens/GroupScreen/Groups";
+import Main from "./mainPage";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import React from "react";
 function App() {
-  const [array, setarray] = useState([]);
-  const [active, setActive] = useState("Personal Info");
   const [islogin, setIsLogin] = useState(true);
   return (
     <>
-      {!islogin && (
-        <>
-          <SignupScreen />
-          <LoginScreen />
-        </>
-      )}
-      <Navbar active={active} setActive={setActive} />
-      {active === "Personal Info" ? <PersonalInfo /> : <Groups groups={array} />}
+      <Routes>
+        <Route path="/" element={ <Main />} />
+      </Routes>
     </>
   );
 }
---
+++ src/mainPage.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import PersonalInfo from "./screens/PersonalInfoScreen/PersonalInfo";
+import Navbar from "./components/Navbar/Navbar";
+import Groups from "./screens/GroupScreen/Groups";
+
+const Main = () => {
+  const [array, setarray] = useState([]);
+  const [active, setActive] = useState("Personal Info");
+  return (
+    <>
+      <Navbar active={active} setActive={setActive} />
+      {active === "Personal Info" ? (
+        <PersonalInfo />
+      ) : (
+        <Groups groups={array} />
+      )}
+    </>
+  );
+};
+
+export default Main;
