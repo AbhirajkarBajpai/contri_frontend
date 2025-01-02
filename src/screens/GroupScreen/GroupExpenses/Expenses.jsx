6c3ba22d816ec82840ba2bd112173545a887ffeb
Abhirajkar Bajpai <21IT3001@rgipt.ac.in>
2025-01-02 18:55:27 +0530

    del successfully added and tested

--- src/screens/GroupScreen/GroupExpenses/Expenses.jsx
+++ src/screens/GroupScreen/GroupExpenses/Expenses.jsx
@@ -121,6 +121,32 @@ const Expenses = () => {
     }
   }
 
+
+  async function handleDeleteExpense(expenseId){
+    try { 
+      const response = await fetch(
+        `https://contri-backend.vercel.app/api/v1/expense/delExpense/${expenseId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("data:", data);
+      alert("Expense Deleted!");
+      setFetchAgain(true);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("something went wrong!");
+    }
+  }
+
   return (
     <div className={styles.ExpensesPage}>
       <img
@@ -176,6 +202,7 @@ const Expenses = () => {
       <div className={styles.expensesContn}>
         {groupData?.expenses?.length > 0 ? (
           groupData.expenses?.map((expense) => (
+            <div className={styles.expense}>
             <div
               onClick={() => {
                 setExpenseId(expense._id);
@@ -197,6 +224,37 @@ const Expenses = () => {
                     : userIdToName[expense.createdBy]}
                 </span>
               </div>
+              </div>
+              {expense.createdBy === loggedInUser && (
+                <svg
+                  className={styles.delExpense}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleDeleteExpense(expense._id);
+                  }}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="#fff"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M6 7V18C6 19.1046 6.89543 20 8 20H16C17.1046 20 18 19.1046 18 18V7M6 7H5M6 7H8M18 7H19M18 7H16M10 11V16M14 11V16M8 7V5C8 3.89543 8.89543 3 10 3H14C15.1046 3 16 3.89543 16 5V7M8 7H16"
+                      stroke="#56d4f7"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
+              )}
             </div>
           ))
         ) : (
